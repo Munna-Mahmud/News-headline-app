4a7062df00cde9cfab8654d659ab847b5b50d122
munna mahmud <learnermunna@gmail.com>
2021-12-14 21:03:50 +0600

    NewsList components done

--- src/App.js
+++ src/App.js
@@ -1,6 +1,33 @@
 import React from "react";
 import Header from "./Components/Header";
+import NewsList from "./Components/NewsList";
 import { newsCatagory } from './News/index';
+
+const fakeData = [
+  {
+    title: "Title",
+    content: "Content",
+    url: "https://linktonews.com",
+    urlToImage: "https://linktoimage.com",
+    publishedAt: 'published date and time',
+    source: {
+      name: 'CNN',
+    },
+
+  },
+  {
+    title: "Title",
+    content: "Content",
+    url: "https://linktonews.com",
+    urlToImage: "https://linktoimage.com",
+    publishedAt: 'published date and time',
+    source: {
+      name: 'CNN',
+
+    },
+  },
+];
+
 
 class App extends React.Component {
   render() {
@@ -8,7 +35,8 @@ class App extends React.Component {
       <div className="container">
         <div className="row">
           <div className="col-sm-6 offset-md-3">
-           <Header catagory ={newsCatagory.sports}></Header>
+            <Header catagory={newsCatagory.sports}></Header>
+            <NewsList news={fakeData}></NewsList>
           </div>
         </div>
       </div>
